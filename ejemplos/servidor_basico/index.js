25b31590f15cc9e0025941b00773e83adde048f2
Isa Gómez <hello@isagomez.com>
2021-01-18 22:21:59 +0100

    Creando un servidor local

---
+++ ejemplos/servidor_basico/index.js
@@ -0,0 +1,12 @@
+// cargar librería http
+const http = require('http');
+
+// definir un servidor
+const server = http.createServer(function (request, response) {
+	response.writeHead(200, { 'Content-type': 'text/html' });
+	response.end('Wake up, <strong>Argg...</strong>')
+});
+
+// arrancar el servidor
+server.listen(1337, '127.0.0.1');
+console.log('Servidor arrancado en http://127.0.0.1:1337');
