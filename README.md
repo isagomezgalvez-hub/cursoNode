cb130ad378070771d35a1cad3f186f0838b57286
Isa Gómez <hello@isagomez.com>
2021-01-28 23:04:19 +0100

    Borrar datos

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Ejercicio Realizado en clase dentro del módulo de Node
 ##Comando arranque MongoDB
 
 ./bin/mongod --dbpath ./data/db
+
+## API Methods
+
+### /api/agentes
+
+Get a list of agentes
+
+
+### /api/agentes/id
+
+Filter agentes by id
+### POST 
+
--- nodeapi/routes/api/agentes.js
+++ nodeapi/routes/api/agentes.js
@@ -1,5 +1,6 @@
 
 var express = require('express');
+const { findOne } = require('../../models/Agente');
 var router = express.Router();
 const Agente = require('../../models/Agente');
 
@@ -12,5 +13,76 @@ router.get('/', async function (req, res, next) {
 		next(err)
 	}
 });
+
+/* GET /api/agentes:id
+Buscar un agente */
+
+router.get('/:id', async function (req, res, next) {
+	try {
+		const _id = req.params.id;
+		const agente = await Agente.findOne({ _id: _id });
+
+		if (!agente) {
+			res.status(404).json({ error: 'not found' });
+			return
+		}
+		res.json({ result: agente });
+
+	} catch (err) {
+		next(err)
+	}
+})
+
+/* POST /api/agentes
+Crear un agente  (body) */
+
+router.post('/', async (req, res, next) => {
+	try {
+		const agenteData = req.body;
+		const agente = new Agente(agenteData)
+
+		const agenteCreado = await agente.save();
+
+		res.status(201).json({ agenteCreado });
+
+	} catch (error) {
+		next(error)
+	}
+})
+
+/* PUT /api/agentes/:id (body)
+Actualizar un agente */
+
+router.put('/:id', async (req, res, next) => {
+	try {
+		const _id = req.params.id
+		const agenteData = req.body
+
+		const agenteActualizado = await Agente.findOneAndUpdate({ _id: _id }, agenteData,
+			{
+				new: true,
+				useFindAndModify: false
+			});
+		//usamos {new: true} para que nos devuelva el agente actualizado
+
+		res.json({ result: agenteActualizado })
+	} catch (error) {
+		next(error);
+	}
+})
+
+/* DELETE /api/agentes/:id (body)
+Elimina un agente */
+
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const _id = req.params.id
+		await Agente.deleteOne({ _id: _id })
+		res.json()
+	} catch (error) {
+		next(error);
+	}
+})
+
 
 module.exports = router;
